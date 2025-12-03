586bc9a919d77a1620e79649f961e23fe472e44a
Tomislav G. <8core.eu@gmail.com>
2025-12-03 11:31:58 +0100

    Updated modSEUP.class.php

--- seup/core/modules/modSEUP.class.php
+++ seup/core/modules/modSEUP.class.php
@@ -79,7 +79,7 @@ class modSEUP extends DolibarrModules
 		$this->editor_squarred_logo = '';					// Must be image filename into the module/img directory followed with @modulename. Example: 'myimage.png@seup'
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-		$this->version = '3.0.2';
+		$this->version = '5.0.2';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
